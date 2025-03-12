60d148c6b4211d11ec9af0876e64f328abcd822b
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-03-12 20:59:12 +0330

    ilia 0.13v

--- aparteman/urls.py
+++ aparteman/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import aparteman
 from functools import partial
 
-
 urlpatterns = [
     path("aparteman/view-royayee", partial(aparteman, 0)),
     path("aparteman/sargarmi", partial(aparteman, 1)),

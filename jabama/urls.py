24f84c951c0f84c6232e7b857ceab5f021e75f22
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-03-12 20:53:19 +0330

    ilia 0.11v

--- jabama/urls.py
+++ jabama/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("aparteman", include("aparteman.urls")),
+    path("boomgardi", include("boomgardi.urls")),
+    path("villa", include("villa.urls")),
+    path("colbe", include("colbe.urls")),
 ]
